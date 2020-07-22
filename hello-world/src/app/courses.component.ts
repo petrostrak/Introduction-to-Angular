cdfb3812e432fefc2f660885cb924b64cd7ec35b
petros <pit.trak@gmail.com>
2020-07-22 19:47:49 +0300

    Property Binding
    <img src="{{ imageUrl }}"/> is compiled to <img [src]="imageUrl"/> by the tsc

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -8,11 +8,13 @@ import { Component } from '@angular/core';
         <ul *ngFor="let course of courses">
             {{ course }}
         </ul>
+        <img src="{{ imageUrl }}" />
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    imageUrl = "https://i.pinimg.com/originals/44/8e/86/448e862e9801febb2fbc65c4d73adfec.jpg";
 
     constructor(service: CoursesService){
         this.courses = service.getCourses();
