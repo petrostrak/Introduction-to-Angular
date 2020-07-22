97e8ec3af28d15c4f57929458c25eaf6272620c3
petros <pit.trak@gmail.com>
2020-07-22 17:18:02 +0300

    ng g c courses

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
